6cfa0095f573bbbf1f2599acd5da82015c3fa992
sefa-akkaya <94069712+sefa-akkaya@users.noreply.github.com>
2024-08-15 11:26:50 +0300

    TASK-11 sdafsdf

--- src/test/java/works/buddy/samples/WorksWithHerokuServletTest.java
+++ src/test/java/works/buddy/samples/WorksWithHerokuServletTest.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintWriter;
-
+ 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 
