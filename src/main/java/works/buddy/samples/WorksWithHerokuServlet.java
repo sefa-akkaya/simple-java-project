eef92299279a8b54c6b268f5783b37bf077991d9
sefa-akkaya <94069712+sefa-akkaya@users.noreply.github.com>
2024-08-15 14:45:21 +0300

    TASK-11 denemetask

--- src/main/java/works/buddy/samples/WorksWithHerokuServlet.java
+++ src/main/java/works/buddy/samples/WorksWithHerokuServlet.java
@@ -11,7 +11,7 @@ public class WorksWithHerokuServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/plain");
-        response.setStatus(404);
+        response.setStatus(404); 
         PrintWriter writer = response.getWriter();
         writer.print("Buddy Works with Heroku");
         writer.close();
