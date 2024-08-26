9f89aec562aaadb22564c9dbab790bb26522c912
sefa-akkaya <94069712+sefa-akkaya@users.noreply.github.com>
2024-08-26 08:34:18 +0300

    TASK-11 sadfsadfdsa

--- src/main/java/works/buddy/samples/WorksWithHerokuServlet.java
+++ src/main/java/works/buddy/samples/WorksWithHerokuServlet.java
@@ -7,6 +7,10 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter; 
     
+
+
+
+
 public class WorksWithHerokuServlet extends HttpServlet { 
     
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
